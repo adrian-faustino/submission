e0ca4a630f02b3f1d97f8a43af8d3e4644b3e03c
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-09-03 00:41:20 -0400

    added timer initial state

--- src/components/EntryCard/Timer/Timer.tsx
+++ src/components/EntryCard/Timer/Timer.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface ITimerState {
+  isRunning: boolean;
+  totalTime: number;
+  startTime: number;
+}
+
+const initialState: ITimerState = {
+  isRunning: false,
+  totalTime: 0,
+  startTime: Date.now(),
+};
 
 const Timer = () => {
+  /* State */
+  const [timer, setTimer] = useState(initialState);
+
   return (
-    <div>
-      <h3>im a timer</h3>
+    <div className="Timer__">
+      <h3>Timer</h3>
+
+      {/* display timer details */}
+      <span>Total: {timer.totalTime}</span>
     </div>
   );
 };
