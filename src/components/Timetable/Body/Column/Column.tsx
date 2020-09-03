e4760e09017435898f8b369be81b647c6a75544a
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-09-02 23:01:31 -0400

    Rendering each hour row

--- src/components/Timetable/Body/Column/Column.tsx
+++ src/components/Timetable/Body/Column/Column.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 /* Constants */
+import { HOURS_PER_DAY } from "../../../../constants/appConfig";
+/* Subcomponents */
+import { EntryCard } from "../../../";
 
 const Column = () => {
   const renderEntryCardJSX = () => {
-    return;
+    return HOURS_PER_DAY.map((hour, i) => (
+      <EntryCard key={`${hour}-${i}-hour`} />
+    ));
   };
 
-  return (
-    <div>
-      <div>component!</div>
-    </div>
-  );
+  return <div>{renderEntryCardJSX()}</div>;
 };
 
 export default Column;
--- src/constants/appConfig.ts
+++ src/constants/appConfig.ts
@@ -13,3 +13,29 @@ export const DAYS_OF_WEEK: Array<Day> = [
 ];
 
 // time of day
+export const HOURS_PER_DAY: Array<number> = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+];
