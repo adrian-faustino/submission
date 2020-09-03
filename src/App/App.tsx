7833937aa1062ae300a93e5144f4280886c300df
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-09-03 04:27:43 -0400

    implementing useContext for weekly total time

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { AllEntriesCTX } from "../context";
 /* Styles */
 import "./App.css";
 /* Views */
 // import { TestView } from "../views";
 /* Subcomponents */
 import { Nav, Timetable } from "../components";
+/* Constants */
+import { IEntry } from "../constants/types";
 
 const App = () => {
+  const [allEntries, setAllEntries] = useState([]);
+
   return (
     <div className="App">
-      <Nav />
-      <Timetable />
+      <AllEntriesCTX.Provider value={allEntries}>
+        <Nav />
+        <Timetable />
+      </AllEntriesCTX.Provider>
 
       {/* <TestView /> */}
     </div>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AllEntriesCTX } from "../../context";
 /* Styles */
 import "./Nav.css";
 
 const Nav = () => {
+  const allEntries = useContext(AllEntriesCTX);
+
   return (
     <section className="Nav__ center-text">
       <h3>Logo</h3>
-      <span>Total minutes this week:</span>
+      <span>Total minutes this week: {allEntries}</span>
     </section>
   );
 };
