58ac55923fe73439658a6d9cc6ca4808736cf2bc
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-09-02 23:17:04 -0400

    basic styling before working on adding new entry

--- src/components/EntryCard/EntryCard.tsx
+++ src/components/EntryCard/EntryCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+/* Styles */
+import "./EntryCard.css";
 /* Constants */
 import { IDay, IHour } from "../../constants/types";
 
@@ -9,7 +11,7 @@ interface IEntryCardProps {
 
 const EntryCard: React.FC<IEntryCardProps> = ({ day, hour }) => {
   return (
-    <div>
+    <div className="EntryCard__ box-shadow border-rad">
       <span>{hour}</span>
       <span>{day.short}</span>
     </div>
