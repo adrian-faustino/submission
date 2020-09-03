cded97299828296d7f8f410a0bd52fbaaf705ae1
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-09-02 22:23:54 -0400

    basic timetable header styling and positioning

--- src/components/Timetable/Headers/Headers.tsx
+++ src/components/Timetable/Headers/Headers.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+/* Styles */
+import "./Headers.css";
 /* Constants */
 import { DAYS_OF_WEEK } from "../../../constants/appConfig";
 
 const Headers = () => {
   const renderHeadersJSX = () => {
     return DAYS_OF_WEEK.map((day, i) => (
-      <span key={`${day}-${i}-header`}>{day.short}</span>
+      <span className="Headers__item" key={`${day}-${i}-header`}>
+        {day.short}
+      </span>
     ));
   };
 
-  return <div>{renderHeadersJSX()}</div>;
+  return (
+    <section className="Headers__ center-text box-shadow">
+      {renderHeadersJSX()}
+    </section>
+  );
 };
 
 export default Headers;
